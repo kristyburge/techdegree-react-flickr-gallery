b7d08d6692b69db80464f2416a85d22e985939f3
kristyburge <kristyburge@gmail.com>
2018-09-17 16:32:40 +0000

    Add content to NotFound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,10 @@ const NotFound = () => {
     
     return(
         <div>
-            <h2>Sorry, page not found</h2>
+            <h2>Oops! You broke the internet!</h2>
+            <p>Just kidding! But, that page can't be found.</p>
+            <p>Why don't you try searching for a photo using the search bar above...</p>
+            <p>Or by clicking a blue button in the navigation above?</p>
         </div>
     );
 }
